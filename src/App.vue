bfdabe87f2b704c81af3eb3ce59b55f47b328d5d
jagerente <JustlPains@gmail.com>
2022-05-14 13:54:34 +0300

    styles updated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <navbar />
-  <div>
+  <div class="container-fluid vh-100 p-0">
+    <navbar />
     <router-view></router-view>
   </div>
 </template>
@@ -20,17 +20,4 @@ export default {
   border-color: red;
   border: 3px dashed red;
 }
-/* * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-} */
-
-// body {
-//   background-color: #1a1927;
-//   color: #a9a8ae;
-// }
-
-// @import "../node_modules/bootstrap/scss/bootstrap";
-// @import "assets/vrising.scss";
 </style>
