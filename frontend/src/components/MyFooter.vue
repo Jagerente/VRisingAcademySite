e3a4f558d52e692f62650e43a936964143a356a0
jagerente <JustlPains@gmail.com>
2022-05-23 22:54:39 +0300

    Footer rework

--- frontend/src/components/MyFooter.vue
+++ frontend/src/components/MyFooter.vue
@@ -1,36 +1,25 @@
 <template>
     <footer v-if="this.$router.currentRoute.value.path !== '/items'" class="d-flex flex-column sticky-bottom h-100">
-        <div class="d-flex justify-content-center redirects mt-auto">
-            <a href="https://vk.com/vrisingacademy" target="blank">
-                <img class="image" :src="
-                    require('@/assets/images/ui/main_page/VK.webp')
-                ">
-            </a>
-            <a href="https://t.me/vrisingacademy" target="blank">
-                <img class="image" :src="
-                    require('@/assets/images/ui/main_page/TG.webp')
-                ">
-            </a>
-            <a href="https://boosty.to/vrisingacademy" target="blank">
-                <img class="image" type="image" :src="
-                    require('@/assets/images/ui/main_page/Boosty.webp')
-                ">
-            </a>
+        <div class="d-flex mb-1 justify-content-center mt-auto">
+            <my-link content="Join VK!" link="https://vk.com/vrisingacademy" icon="vk" />
+            <my-link content="Join Telegram!" link="https://t.me/vrisingacademy" icon="tg" />
+            <my-link content="Join Discord!" link="https://discord.gg/7jmczsbrCH" icon="discord" />
+            <my-link content="Support us!" link="https://boosty.to/vrisingacademy" icon="boosty" />
         </div>
         <p class="text-center">© 2022 V Rising Academy · Genshin Academy Team</p>
     </footer>
 </template>
 
 <script>
+import MyLink from "@/components/Footer/MyLink";
+
 export default {
+    components: {
+        MyLink
+    },
     name: "my-footer",
 };
 </script>
 
-<style scoped>
-.image {
-    width: 30px;
-    margin-right: 5px;
-    margin-left: 5px;
-}
+<style>
 </style>
