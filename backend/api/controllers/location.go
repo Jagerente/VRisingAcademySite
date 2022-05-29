d654358b9c113b9635f537368e5388a471fcd39e
Order <43047529+aelariane@users.noreply.github.com>
2022-05-29 19:21:50 +0300

    Implemented locations endpoints

--- backend/api/controllers/location.go
+++ backend/api/controllers/location.go
@@ -3,19 +3,19 @@ package controllers
 import (
 	//"net/http"
 	"VRisingAcademySite/database"
+	"VRisingAcademySite/models"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
 )
 
-func getLocationList(ctx *gin.Context) {
-
-	var items = make([]ItemJson, 0)
+func getRegionList(ctx *gin.Context) {
+	var items = make([]models.Region, 0)
 	connection := database.CreateConnection()
 	defer connection.Close()
-	query := "select * from items"
+	query := `select * from regions`
 
-	fmt.Println(query)
 	rows, err := connection.Query(query)
 
 	if err != nil {
@@ -25,8 +25,55 @@ func getLocationList(ctx *gin.Context) {
 	defer rows.Close()
 
 	for rows.Next() {
-		item := ItemJson{}
-		readError := rows.Scan(&item.Id, &item.Name, &item.Description, &item.Tier, &item.Type)
+		item := models.Region{}
+		readError := rows.Scan(&item.Id, &item.Name)
+		if readError != nil {
+			fmt.Print(readError)
+			continue
+		}
+		items = append(items, item)
+	}
+
+	ctx.JSON(200, items)
+}
+
+func getLocationList(ctx *gin.Context) {
+
+	var items = make([]models.Location, 0)
+	connection := database.CreateConnection()
+	defer connection.Close()
+	query := `select
+    locations.id,
+    locations.name,
+    locations.description,
+    locations.mapgenieid,
+    regions.name,
+    array_agg(itemlocations.itemid)
+from
+    locations
+    join regions on regions.id = locations.id
+    join itemlocations on itemlocations.locationid = locations.id
+group by
+    locations.id,
+    regions.name`
+
+	rows, err := connection.Query(query)
+
+	if err != nil {
+		fmt.Print(err)
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		item := models.Location{}
+		readError := rows.Scan(
+			&item.Id,
+			&item.Name,
+			&item.Description,
+			&item.MapgenieId,
+			&item.Region,
+			pq.Array(&item.Loot))
 		if readError != nil {
 			fmt.Print(readError)
 			continue
@@ -39,4 +86,5 @@ func getLocationList(ctx *gin.Context) {
 
 func HandleLocationRequest(ctx *gin.RouterGroup) {
 	ctx.GET("/list", getLocationList)
+	ctx.GET("/regions", getRegionList)
 }
