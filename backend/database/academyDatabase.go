c5df6d91d660dff40099e9c05d258adda79f7721
Order <43047529+aelariane@users.noreply.github.com>
2022-05-31 23:40:13 +0300

    Updated database

--- backend/database/academyDatabase.go
+++ backend/database/academyDatabase.go
@@ -16,7 +16,7 @@ const (
 	postgresPassword         string = "83"
 	postgresHost             string = "localhost"
 	postgresPort             int32  = 5432
-	DatabaseVersion          int32  = 5
+	DatabaseVersion          int32  = 6
 )
 
 func CheckIfDatabaseNeedsUpdate() bool {
