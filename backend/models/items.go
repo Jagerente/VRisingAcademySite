fe72737025ab7c89c743d04b2b5f831ffe9d09d8
Order <43047529+aelariane@users.noreply.github.com>
2022-06-05 20:10:20 +0300

    Updated models

--- backend/models/items.go
+++ backend/models/items.go
@@ -7,27 +7,34 @@ const (
 
 type ItemType struct {
 	Entity
-	Title string `json:"title"`
+	Title string `json:"name"`
+}
+
+type NewItemSet struct {
+	Id          int32  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 type NewItem struct {
 	GameEntity
-	Tier        int32    `json:"tier"`
-	Type        int32    `json:"typeid"`
-	TypeName    string   `json:"type"`
-	KnowledgeId *int32   `json:"knowledgeid"`
-	Stations    []int32  `json:"stations"`
-	Recipes     []int32  `json:"recipes"`
-	ReagentFor  []int32  `json:"reagentFor"`
-	Tags        []string `json:"tags"`
-	Durability  *int32   `json:"durability"`
-	GearLevel   *int32   `json:"gearLevel"`
-	MainStat    *float64 `json:"mainStat"`
-	BonusStats  []string `json:"bonusStats"`
-	SetId       *int32   `json:"setId"`
-	Set         *string  `json:"set"`
-	SlotId      *int32   `json:"slotId"`
-	Locations   []string `json:"locations"`
+	Tier        int32       `json:"tier"`
+	Type        ItemType    `json:"type"`
+	KnowledgeId *int32      `json:"knowledge"`
+	Stations    []int32     `json:"stations"`
+	Recipes     []int32     `json:"recipes"`
+	ReagentFor  []int32     `json:"reagentFor"`
+	Tags        []string    `json:"tags"`
+	Durability  *int32      `json:"durability"`
+	GearLevel   *int32      `json:"gearLevel"`
+	MainStat    *float64    `json:"mainStat"`
+	BonusStats  []string    `json:"bonusStats"`
+	SetId       *int32      `json:"-"`
+	Set         *NewItemSet `json:"set"`
+	SlotId      *int32      `json:"slot"`
+	Locations   []int32     `json:"locations"`
+	Variants    []int32     `json:"variants"`
+	OwnerId     *int32      `json:"owner"`
 }
 
 type Item struct {
