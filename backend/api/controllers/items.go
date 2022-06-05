65027a2a7a00d13962d05c97cba3d7dbe5090878
Order <43047529+aelariane@users.noreply.github.com>
2022-06-05 22:28:42 +0300

    Updated items, removed owner field

--- backend/api/controllers/items.go
+++ backend/api/controllers/items.go
@@ -21,8 +21,8 @@ type ItemJson struct {
 type ListResponse struct {
 }
 
-func getItemTypesList() []models.ItemType {
-	var items = make([]models.ItemType, 0)
+func getItemTypesList() []models.ItemTypeObject {
+	var items = make([]models.ItemTypeObject, 0)
 	connection := database.CreateConnection()
 	defer connection.Close()
 
@@ -37,7 +37,7 @@ func getItemTypesList() []models.ItemType {
 	defer rows.Close()
 
 	for rows.Next() {
-		item := models.ItemType{}
+		item := models.ItemTypeObject{}
 
 		readError := rows.Scan(
 			&item.Id,
@@ -56,7 +56,7 @@ func getItemTypesList() []models.ItemType {
 
 func handleItemTypesList(ctx *gin.Context) {
 
-	var items = make([]models.ItemType, 0)
+	var items = make([]models.ItemTypeObject, 0)
 	connection := database.CreateConnection()
 	defer connection.Close()
 
@@ -71,7 +71,7 @@ func handleItemTypesList(ctx *gin.Context) {
 	defer rows.Close()
 
 	for rows.Next() {
-		item := models.ItemType{}
+		item := models.ItemTypeObject{}
 
 		readError := rows.Scan(
 			&item.Id,
@@ -88,7 +88,7 @@ func handleItemTypesList(ctx *gin.Context) {
 }
 
 func handleListRequest(ctx *gin.Context) {
-	var response = make([]models.NewItem, 0)
+	var response = make([]models.Item, 0)
 	connection := database.CreateConnection()
 	defer connection.Close()
 
@@ -184,8 +184,7 @@ func handleListRequest(ctx *gin.Context) {
             order by
                 structurevariants.variantid
         )
-    ) as variants,
-    items.ownerid
+    ) as variants
 from
     items
     join itemtypes on itemtypes.id = items.type full
@@ -217,7 +216,7 @@ order by
 	}
 	defer rows.Close()
 	for rows.Next() {
-		item := models.NewItem{
+		item := models.Item{
 			Stations:   make([]int32, 0),
 			Recipes:    make([]int32, 0),
 			ReagentFor: make([]int32, 0),
@@ -225,7 +224,7 @@ order by
 			BonusStats: make([]string, 0),
 			Locations:  make([]int32, 0),
 			Variants:   make([]int32, 0),
-			Type:       models.ItemType{}}
+			Type:       models.ItemTypeObject{}}
 
 		var setName *string
 		var setDesc *string
@@ -251,15 +250,14 @@ order by
 			&item.SlotId,
 			pq.Array(&item.BonusStats),
 			pq.Array(&item.Locations),
-			pq.Array(&item.Variants),
-			&item.OwnerId)
+			pq.Array(&item.Variants))
 
 		if readError != nil {
 			fmt.Print(readError)
 			continue
 		}
 		if item.SetId != nil {
-			item.Set = &models.NewItemSet{
+			item.Set = &models.ItemSetObject{
 				Id:          *item.SetId,
 				Name:        *setName,
 				Description: *setDesc}
@@ -272,14 +270,14 @@ order by
 
 func handleGroupedItemsListRequest(ctx *gin.Context) {
 	type ResponseItem struct {
-		Id    int32            `json:"id"`
-		Name  string           `json:"name"`
-		Items []models.NewItem `json:"items"`
+		Id    int32         `json:"id"`
+		Name  string        `json:"name"`
+		Items []models.Item `json:"items"`
 	}
 	type Response struct {
-		Id    int32          `json:"id"`
-		Name  string         `json:"name"`
-		Items []ResponseItem `json:"sets"`
+		Id   int32          `json:"id"`
+		Name string         `json:"name"`
+		Sets []ResponseItem `json:"sets"`
 	}
 
 	var response = make([]Response, 0)
@@ -287,13 +285,13 @@ func handleGroupedItemsListRequest(ctx *gin.Context) {
 
 	for _, item := range types {
 		toAdd := Response{
-			Id:    item.Id,
-			Name:  item.Title,
-			Items: make([]ResponseItem, 0)}
+			Id:   item.Id,
+			Name: item.Title,
+			Sets: make([]ResponseItem, 0)}
 		response = append(response, toAdd)
 	}
 
-	type ResponseMapItem map[string][]models.NewItem
+	type ResponseMapItem map[string][]models.Item
 	type ResponseMap map[string]*ResponseMapItem
 	const UnsetTypeId int32 = -1
 
@@ -407,8 +405,7 @@ func handleGroupedItemsListRequest(ctx *gin.Context) {
             order by
                 structurevariants.variantid
         )
-    ) as variants,
-    items.ownerid
+    ) as variants
 from
     items
     join itemtypes on itemtypes.id = items.type full
@@ -440,7 +437,7 @@ order by
 	}
 	defer rows.Close()
 	for rows.Next() {
-		item := models.NewItem{
+		item := models.Item{
 			Stations:   make([]int32, 0),
 			Recipes:    make([]int32, 0),
 			ReagentFor: make([]int32, 0),
@@ -448,7 +445,7 @@ order by
 			BonusStats: make([]string, 0),
 			Locations:  make([]int32, 0),
 			Variants:   make([]int32, 0),
-			Type:       models.ItemType{}}
+			Type:       models.ItemTypeObject{}}
 
 		var setName *string
 		var setDesc *string
@@ -474,8 +471,7 @@ order by
 			&item.SlotId,
 			pq.Array(&item.BonusStats),
 			pq.Array(&item.Locations),
-			pq.Array(&item.Variants),
-			&item.OwnerId)
+			pq.Array(&item.Variants))
 
 		if readError != nil {
 			fmt.Print(readError)
@@ -485,7 +481,7 @@ order by
 		var key int32 = UnsetTypeId
 		if item.SetId != nil {
 			key = *item.SetId
-			item.Set = &models.NewItemSet{
+			item.Set = &models.ItemSetObject{
 				Id:          key,
 				Name:        *setName,
 				Description: *setDesc}
@@ -496,7 +492,7 @@ order by
 			updatedArray := append((*responseItem)[setMap[key]], item)
 			(*responseItem)[setMap[key]] = updatedArray
 		} else {
-			arayToAdd := []models.NewItem{item}
+			arayToAdd := []models.Item{item}
 			(*responseItem)[setMap[key]] = arayToAdd
 		}
 	}
@@ -512,12 +508,12 @@ order by
 				newVal = ResponseItem{
 					Id:    valueValue[0].Set.Id,
 					Name:  valueValue[0].Set.Name,
-					Items: make([]models.NewItem, 0)}
+					Items: make([]models.Item, 0)}
 			}
 			for _, itemValue := range valueValue {
 				newVal.Items = append(newVal.Items, itemValue)
 			}
-			response[i].Items = append(response[i].Items, newVal)
+			response[i].Sets = append(response[i].Sets, newVal)
 		}
 		i++
 	}
