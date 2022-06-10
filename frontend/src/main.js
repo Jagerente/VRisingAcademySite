9494559daabcbfdc1d42ab9617985c34b6acf63a
jagerente <JustlPains@gmail.com>
2022-06-10 21:00:07 +0300

    Markdown support added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,23 @@
-import { createApp } from 'vue'
-import App from './App'
-import components from '@/components/UI'
-import router from "@/router/router"
-import store from '@/store'
+import { createApp } from 'vue';
+import App from './App';
+import components from '@/components/UI';
+import router from '@/router/router';
+import store from '@/store';
 import Popper from "vue3-popper";
-import "bootstrap/dist/js/bootstrap.js"
-import "@/assets/vrising.scss"
-import "@/assets/styles/va_variables.scss";
 
-const app = createApp(App)
+import "bootstrap/dist/js/bootstrap.js";
+import "@/assets/styles/va_variables.scss";
+import "@/assets/vrising.scss";
+import "@/assets/styles/va_styles.scss";
+
+const app = createApp(App);
 
 components.forEach(component => {
     app.component(component.name, component)
-})
+});
 
 app
     .use(router)
     .use(store)
     .use(Popper)
-    .mount('#app')
+    .mount('#app');
