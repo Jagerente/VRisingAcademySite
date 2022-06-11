16d2c9fc160ea1f98ff9d0219b218a1590a79a9b
Order <43047529+aelariane@users.noreply.github.com>
2022-06-12 00:02:16 +0300

    Updated items DB query, added V2 option

--- backend/api/controllers/items.go
+++ backend/api/controllers/items.go
@@ -6,8 +6,209 @@ import (
 	"VRisingAcademySite/models"
 	"fmt"
 
+	"database/sql"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
+)
+
+const (
+	ItemFullQuery string = `select
+    items.id,
+    items.name,
+    items.description,
+    items.tier,
+    items.type as typeid,
+    itemtypes.title as typetitle,
+    items.knowledgeid,
+    array(
+        (
+            select
+                stcns.id
+            from
+                recipestations as rcst
+                join stations as stcns on rcst.stationId = stcns.id
+            where
+                rcst.recipeId = rcp.id
+        )
+    ) as stations,
+    array(
+        (
+            select
+                recipes.id
+            from
+                reciperesults
+                join recipes on recipes.id = reciperesults.recipeid
+            where
+                reciperesults.itemid = items.id
+        )
+    ) as recipes,
+    array(
+        (
+            select
+                rc.id
+            from
+                recipeingredients as rci
+                join recipes as rc on rci.recipeId = rc.id
+            where
+                rci.itemId = items.id
+        )
+    ) as reagentFor,
+    array(
+        (
+            select
+                tgs.value
+            from
+                itemtags as itt
+                join tags as tgs on itt.tagId = tgs.id
+            where
+                itt.itemId = items.id
+        )
+    ) as tags,
+    stats.durability,
+    stats.gearLevel,
+    stats.mainStat,
+    items.setid,
+    sets.name,
+    sets.description,
+    stats.slotid,
+    array(
+        (
+            select
+                secondarystats.bonus
+            from
+                secondaryitemstats
+                join secondarystats on secondarystats.id = secondaryitemstats.secondarystatid
+            where
+                secondaryitemstats.statsId = stats.id
+        )
+    ) as bonusStats,
+    array(
+        (
+            select
+                lc.id
+            from
+                itemlocations as itl
+                join locations as lc on itl.locationId = lc.id
+            where
+                itl.itemId = items.id
+        )
+    ) as locations,
+    array(
+        (
+            select
+                structurevariants.variantid
+            from
+                structurevariants
+            where
+                structurevariants.structureid = items.id
+            order by
+                structurevariants.variantid
+        )
+    ) as variants,
+    array (
+        (
+            select
+                salvageables.id
+            from
+                salvageables
+            where
+                salvageables.itemid = items.id
+        )
+    ) as salvageables,
+    array (
+        (
+            select
+                salvageableresults.salvageableid
+            from
+                salvageableresults
+            where
+                salvageableresults.itemid = items.id
+        )
+    ) as salvageableOf
+from
+    items
+    join itemtypes on itemtypes.id = items.type full
+    join recipeingredients on recipeingredients.itemid = items.id full
+    join recipes as rcp on recipeingredients.itemid = rcp.id full
+    join itemstats as stats on stats.id = items.id full
+    join sets on sets.id = items.setid
+where
+    items.id IS NOT NULL
+group by
+    items.id,
+    itemtypes.title,
+    rcp.id,
+    stats.id,
+    stats.durability,
+    stats.gearLevel,
+    stats.mainStat,
+    items.setid,
+    sets.name,
+    sets.description
+order by
+    items.id,
+    items.type,
+    items.setid`
+
+	ItemFullQueryV2 string = `select
+    items.id,
+    items.name,
+    items.description,
+    items.tier,
+    items.type as typeid,
+    itemtypes.title as typetitle,
+    items.knowledgeid,
+    array_agg(distinct recipestations.stationid) as stations,
+    array_agg(distinct reciperesults.recipeid) as recipes,
+    array_agg(distinct recipeingredients.recipeid) as reagentFor,
+    array_agg(distinct tags.value) as tags,
+    stats.durability,
+    stats.gearLevel,
+    stats.mainStat,
+    items.setid,
+    sets.name,
+    sets.description,
+    stats.slotid,
+    array_agg(distinct secondarystats.bonus) as bonusStats,
+    array_agg(distinct itemlocations.locationid) as locations,
+    array_agg(distinct structurevariants.variantid) as variants,
+    array_agg(distinct salvageables.id) as salvageables,
+    array_agg(distinct salvageableresults.salvageableid) as salvageableOf
+from
+    items
+    join itemtypes on itemtypes.id = items.type
+    full join recipeingredients on recipeingredients.itemid = items.id
+    full join recipes as rcp on recipeingredients.itemid = rcp.id
+    full join recipestations on recipestations.recipeid = rcp.id
+    full join reciperesults on reciperesults.itemid = items.id
+    full join itemstats as stats on stats.id = items.id
+    full join sets on sets.id = items.setid
+    full join itemtags on itemtags.itemid=items.id
+    full join tags on tags.id = itemtags.tagid
+    full join salvageables on salvageables.itemid = items.id
+    full join salvageableresults on salvageableresults.itemid = items.id
+    full join itemlocations on itemlocations.itemid = items.id
+    full join structurevariants on structurevariants.structureid = items.id
+    full join secondaryitemstats on secondaryitemstats.statsid = stats.id
+    full join secondarystats on secondarystats.id = secondaryitemstats.secondarystatid
+where
+    items.id IS NOT NULL
+group by
+    items.id,
+    itemtypes.title,
+    rcp.id,
+    stats.id,
+    stats.durability,
+    stats.gearLevel,
+    stats.mainStat,
+    items.setid,
+    sets.name,
+    sets.description
+order by
+    items.id,
+    items.type,
+    items.setid`
 )
 
 type ItemJson struct {
@@ -88,149 +289,16 @@ func handleItemTypesList(ctx *gin.Context) {
 }
 
 func handleListRequest(ctx *gin.Context) {
+	var query string = ItemFullQuery
+	if ctx.Query("v2") != "" {
+		query = ItemFullQueryV2
+	}
+
 	var response = make([]models.Item, 0)
 	connection := database.CreateConnection()
 	defer connection.Close()
 
-	newQuery := `select
-    items.id,
-    items.name,
-    items.description,
-    items.tier,
-    items.type as typeid,
-    itemtypes.title as typetitle,
-    items.knowledgeid,
-    array(
-        (
-            select
-                stcns.id
-            from
-                recipestations as rcst
-                join stations as stcns on rcst.stationId = stcns.id
-            where
-                rcst.recipeId = rcp.id
-        )
-    ) as stations,
-    array(
-        (
-            select
-                recipes.id
-            from
-                reciperesults
-                join recipes on recipes.id = reciperesults.recipeid
-            where
-                reciperesults.itemid = items.id
-        )
-    ) as recipes,
-    array(
-        (
-            select
-                rc.id
-            from
-                recipeingredients as rci
-                join recipes as rc on rci.recipeId = rc.id
-            where
-                rci.itemId = items.id
-        )
-    ) as reagentFor,
-    array(
-        (
-            select
-                tgs.value
-            from
-                itemtags as itt
-                join tags as tgs on itt.tagId = tgs.id
-            where
-                itt.itemId = items.id
-        )
-    ) as tags,
-    stats.durability,
-    stats.gearLevel,
-    stats.mainStat,
-    items.setid,
-    sets.name,
-    sets.description,
-    stats.slotid,
-    array(
-        (
-            select
-                secondarystats.bonus
-            from
-                secondaryitemstats
-                join secondarystats on secondarystats.id = secondaryitemstats.secondarystatid
-            where
-                secondaryitemstats.statsId = stats.id
-        )
-    ) as bonusStats,
-    array(
-        (
-            select
-                lc.id
-            from
-                itemlocations as itl
-                join locations as lc on itl.locationId = lc.id
-            where
-                itl.itemId = items.id
-        )
-    ) as locations,
-    array(
-        (
-            select
-                structurevariants.variantid
-            from
-                structurevariants
-            where
-                structurevariants.structureid = items.id
-            order by
-                structurevariants.variantid
-        )
-    ) as variants,
-    array (
-        (
-            select
-                salvageables.id
-            from
-                salvageables
-            where
-                salvageables.itemid = items.id
-        )
-    ) as salvageables,
-    array (
-        (
-            select
-                salvageableresults.salvageableid
-            from
-                salvageableresults
-            where
-                salvageableresults.itemid = items.id
-        )
-    ) as salvageableOf
-from
-    items
-    join itemtypes on itemtypes.id = items.type full
-    join recipeingredients on recipeingredients.itemid = items.id full
-    join recipes as rcp on recipeingredients.itemid = rcp.id full
-    join itemstats as stats on stats.id = items.id full
-    join sets on sets.id = items.setid
-where
-    items.id IS NOT NULL
-group by
-    items.id,
-    itemtypes.title,
-    rcp.id,
-    stats.id,
-    stats.durability,
-    stats.gearLevel,
-    stats.mainStat,
-    items.setid,
-    sets.name,
-    sets.description
-order by
-    items.id,
-    items.type,
-    items.setid`
-
-	rows, err := connection.Query(newQuery)
+	rows, err := connection.Query(query)
 
 	if err != nil {
 		fmt.Print(err)
@@ -239,15 +307,15 @@ order by
 	defer rows.Close()
 	for rows.Next() {
 		item := models.Item{
-			Stations:      make([]int32, 0),
-			Recipes:       make([]int32, 0),
-			ReagentFor:    make([]int32, 0),
+			Stations:      make([]sql.NullInt32, 0),
+			Recipes:       make([]sql.NullInt32, 0),
+			ReagentFor:    make([]sql.NullInt32, 0),
 			Tags:          make([]string, 0),
-			BonusStats:    make([]string, 0),
-			Locations:     make([]int32, 0),
-			Variants:      make([]int32, 0),
-			Salvageables:  make([]int32, 0),
-			SalvageableOf: make([]int32, 0),
+			BonusStats:    make([]sql.NullString, 0),
+			Locations:     make([]sql.NullInt32, 0),
+			Variants:      make([]sql.NullInt32, 0),
+			Salvageables:  make([]sql.NullInt32, 0),
+			SalvageableOf: make([]sql.NullInt32, 0),
 			Type:          models.ItemTypeObject{}}
 
 		var setName *string
@@ -324,6 +392,10 @@ func findWeaponIndex(response []ItemGroupedListResponse, weaponType int32) int32
 }
 
 func handleGroupedItemsListRequest(ctx *gin.Context) {
+	var query string = ItemFullQuery
+	if ctx.Query("v2") != "" {
+		query = ItemFullQueryV2
+	}
 
 	var response = make([]ItemGroupedListResponse, 0)
 	types := getItemTypesList()
@@ -343,145 +415,7 @@ func handleGroupedItemsListRequest(ctx *gin.Context) {
 	connection := database.CreateConnection()
 	defer connection.Close()
 
-	newQuery := `select
-    items.id,
-    items.name,
-    items.description,
-    items.tier,
-    items.type as typeid,
-    itemtypes.title as typetitle,
-    items.knowledgeid,
-    array(
-        (
-            select
-                stcns.id
-            from
-                recipestations as rcst
-                join stations as stcns on rcst.stationId = stcns.id
-            where
-                rcst.recipeId = rcp.id
-        )
-    ) as stations,
-    array(
-        (
-            select
-                recipes.id
-            from
-                reciperesults
-                join recipes on recipes.id = reciperesults.recipeid
-            where
-                reciperesults.itemid = items.id
-        )
-    ) as recipes,
-    array(
-        (
-            select
-                rc.id
-            from
-                recipeingredients as rci
-                join recipes as rc on rci.recipeId = rc.id
-            where
-                rci.itemId = items.id
-        )
-    ) as reagentFor,
-    array(
-        (
-            select
-                tgs.value
-            from
-                itemtags as itt
-                join tags as tgs on itt.tagId = tgs.id
-            where
-                itt.itemId = items.id
-        )
-    ) as tags,
-    stats.durability,
-    stats.gearLevel,
-    stats.mainStat,
-    items.setid,
-    sets.name,
-    sets.description,
-    stats.slotid,
-    array(
-        (
-            select
-                secondarystats.bonus
-            from
-                secondaryitemstats
-                join secondarystats on secondarystats.id = secondaryitemstats.secondarystatid
-            where
-                secondaryitemstats.statsId = stats.id
-        )
-    ) as bonusStats,
-    array(
-        (
-            select
-                lc.id
-            from
-                itemlocations as itl
-                join locations as lc on itl.locationId = lc.id
-            where
-                itl.itemId = items.id
-        )
-    ) as locations,
-    array(
-        (
-            select
-                structurevariants.variantid
-            from
-                structurevariants
-            where
-                structurevariants.structureid = items.id
-            order by
-                structurevariants.variantid
-        )
-    ) as variants,
-    array (
-        (
-            select
-                salvageables.id
-            from
-                salvageables
-            where
-                salvageables.itemid = items.id
-        )
-    ) as salvageables,
-    array (
-        (
-            select
-                salvageableresults.salvageableid
-            from
-                salvageableresults
-            where
-                salvageableresults.itemid = items.id
-        )
-    ) as salvageableOf
-from
-    items
-    join itemtypes on itemtypes.id = items.type full
-    join recipeingredients on recipeingredients.itemid = items.id full
-    join recipes as rcp on recipeingredients.itemid = rcp.id full
-    join itemstats as stats on stats.id = items.id full
-    join sets on sets.id = items.setid
-where
-    items.id IS NOT NULL
-group by
-    items.id,
-    itemtypes.title,
-    rcp.id,
-    stats.id,
-    stats.durability,
-    stats.gearLevel,
-    stats.mainStat,
-    items.setid,
-    sets.name,
-    sets.description
-order by
-    items.id,
-    items.type,
-    items.setid`
-
-	rows, err := connection.Query(newQuery)
+	rows, err := connection.Query(query)
 
 	if err != nil {
 		fmt.Print(err)
@@ -490,15 +424,15 @@ order by
 	defer rows.Close()
 	for rows.Next() {
 		item := models.Item{
-			Stations:      make([]int32, 0),
-			Recipes:       make([]int32, 0),
-			ReagentFor:    make([]int32, 0),
+			Stations:      make([]sql.NullInt32, 0),
+			Recipes:       make([]sql.NullInt32, 0),
+			ReagentFor:    make([]sql.NullInt32, 0),
 			Tags:          make([]string, 0),
-			BonusStats:    make([]string, 0),
-			Locations:     make([]int32, 0),
-			Variants:      make([]int32, 0),
-			Salvageables:  make([]int32, 0),
-			SalvageableOf: make([]int32, 0),
+			BonusStats:    make([]sql.NullString, 0),
+			Locations:     make([]sql.NullInt32, 0),
+			Variants:      make([]sql.NullInt32, 0),
+			Salvageables:  make([]sql.NullInt32, 0),
+			SalvageableOf: make([]sql.NullInt32, 0),
 			Type:          models.ItemTypeObject{}}
 
 		var setName *string
