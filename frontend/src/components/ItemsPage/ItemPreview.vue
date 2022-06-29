9d07aa4f003cc981b1a6066c32b5b6765815459f
jagerente <JustlPains@gmail.com>
2022-06-29 15:12:55 +0300

    Removed bootstrap

--- frontend/src/components/ItemsPage/ItemPreview.vue
+++ frontend/src/components/ItemsPage/ItemPreview.vue
@@ -1,16 +1,17 @@
 <template>
   <div
-    class="preview__group"
+    class="preview"
     :class="text != null ? '' : ''"
   >
     <img
-      class="rounded"
+      class="preview__item"
       :class="this.style"
       :disabled="button"
       draggable="false"
       :title="item.name"
       :src="require('@/' + this.imagePath)"
       @click="selectItem(this.items.find(item => { return item.id == this.item.id }))"
+      role="button"
     >
     <div
       v-if="this.text"
@@ -52,93 +53,82 @@ export default {
 
 
 <style scoped lang="scss">
-@import 'bootstrap/scss/_functions.scss';
-@import 'bootstrap/scss/_variables.scss';
-@import 'bootstrap/scss/_mixins.scss';
+@import "@/assets/styles/utility/vars.scss";
 
-@include media-breakpoint-down(sm) {
-  .preview-md {
-    $size: 3em;
-    $margin: 2px;
-    width: $size;
-    height: $size;
-    margin: $margin;
-  }
-
-  .preview-lg {
-    $size: 100px;
-    width: $size;
-    height: $size;
-  }
-}
-
-@include media-breakpoint-up(sm) {
-
-  .preview-md {
-    $size: 5em;
-    $margin: 5px;
-    width: $size;
-    height: $size;
-    margin: $margin;
-  }
-
-  .preview-lg {
-    $size: 200px;
-    width: $size;
-    height: $size;
-  }
-}
-
-.preview-sm {
-  $size: 48px;
-  background: rgba(0, 0, 0, 0.5);
-  font-family: sans-serif;
-  width: $size;
-  height: $size;
-  -webkit-user-drag: none;
-  border: 0;
-  user-select: none;
-  transition: box-shadow 0.15s ease-in-out;
-  color: white;
-  text-align: right;
-}
-
-.preview-sm:hover {
-  box-shadow: 0 0 8px black;
-}
-
-.preview-md {
-  background: rgba(0, 0, 0, 0.5);
-  font-family: sans-serif;
-  -webkit-user-drag: none;
-  border: 0;
-  user-select: none;
-  transition: box-shadow 0.15s ease-in-out;
-}
-
-.preview-md:hover {
-  box-shadow: 0 0 8px black;
-}
-
-.active {
-  border: 1px solid white;
-}
-
-.active:hover {
-  box-shadow: none
-}
-
-.preview__text {
-  pointer-events: none;
-  position: absolute;
-  bottom: 0px;
-  right: 0px;
-  font-size: 16px;
-}
-
-.preview__group {
+.preview {
   position: relative;
   text-align: center;
   color: white;
+
+  &__text {
+    pointer-events: none;
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+    font-size: 16px;
+  }
+
+  &__item {
+    background: $dark;
+    font-family: sans-serif;
+    border: 1px solid $dark;
+    border-radius: 3px;
+    -webkit-user-drag: none;
+    user-select: none;
+    transition: border 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+    color: white;
+    text-align: right;
+
+    &:hover {
+      box-shadow: 0 0 8px black;
+    }
+
+    &.active {
+      border: 1px solid white;
+
+      &:hover {
+        box-shadow: none
+      }
+    }
+  }
+
+  .item-sm {
+    $item-size: 2.5rem;
+    width: $item-size;
+    height: $item-size;
+  }
+
+  .item-md {
+    $item-size: 3rem;
+    width: $item-size;
+    height: $item-size;
+    margin: 2px;
+
+    @media (min-width: $sm) {
+      $item-size: 5rem;
+      width: $item-size;
+      height: $item-size;
+      margin: 5px;
+    }
+  }
+
+  .item-lg {
+    background: none;
+    $item-size: 10rem;
+    width: $item-size;
+    height: $item-size;
+    border: none;
+    pointer-events: none;
+
+    &:hover {
+      box-shadow: none;
+    }
+
+    @media (min-width: $sm) {
+      $item-size: 13rem;
+      width: $item-size;
+      height: $item-size;
+    }
+  }
 }
 </style>
