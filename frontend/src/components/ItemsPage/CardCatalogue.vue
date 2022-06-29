f997d5f525dd34ecd8d94d61a4db28dfac48dc93
jagerente <JustlPains@gmail.com>
2022-06-29 14:18:41 +0300

    Removed obsolete

--- frontend/src/components/ItemsPage/CardCatalogue.vue
+++ frontend/src/components/ItemsPage/CardCatalogue.vue
@@ -3,21 +3,7 @@
     <template #header>
       <filter-input></filter-input>
     </template>
-    <!-- <div
-      class="tab-content d-flex justify-content-center flex-wrap"
-      id="v-pills-tabContent"
-    >
-      <div
-        v-for="itemType in this.itemsGrouped"
-        class="tab-pane fade show w-100"
-        :class="itemType.id === 1 ? 'active' : ''"
-        :id="`v-pills-${itemType.name.toLowerCase()}`"
-        role="tabpanel"
-        :aria-labelledby="`v-pills-${itemType.name.toLowerCase()}-tab`"
-      > -->
     <items-list :sets="this.itemsGrouped[this.selectedType].sets"></items-list>
-    <!-- </div>
-    </div> -->
   </my-card>
 </template>
 
