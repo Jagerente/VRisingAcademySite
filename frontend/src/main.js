f3bd1c323a4736e80baa7e967d9f6e30a1d6d7a1
jagerente <JustlPains@gmail.com>
2022-07-02 23:10:45 +0300

    Removed unused

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,11 +5,6 @@ import customComponents from '@/components/custom';
 import router from '@/router/router';
 import store from '@/store';
 
-// import "bootstrap/dist/js/bootstrap.js";
-// import "@/assets/styles/va_variables.scss";
-// import "@/assets/styles/va_styles.scss";
-// import "@/assets/styles/utility/va_slider.scss";
-
 const app = createApp(App);
 
 components.forEach(component => {
