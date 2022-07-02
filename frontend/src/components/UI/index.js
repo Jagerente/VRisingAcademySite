f125727d799d76b8c034d3e7bb859b8e29bf57ac
jagerente <JustlPains@gmail.com>
2022-07-02 20:39:20 +0300

    Added Swticher

--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -2,6 +2,7 @@ import MyButton from "@/components/UI/MyButton";
 import MyInput from "@/components/UI/MyInput";
 import MySelect from "@/components/UI/MySelect";
 import MyCard from "@/components/UI/MyCard";
+import MySwitch from "@/components/UI/MySwitch";
 import IconBar from "@/components/UI/IconBar";
 import IconCatalogue from "@/components/UI/IconCatalogue";
 import IconFilter from "@/components/UI/IconFilter";
@@ -11,6 +12,7 @@ export default [
   MyInput,
   MySelect,
   MyCard,
+  MySwitch,
   IconBar,
   IconCatalogue,
   IconFilter,
