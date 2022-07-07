0ebd4177651c1019eca745739fa12bd32ece0ee3
jagerente <JustlPains@gmail.com>
2022-07-07 21:52:14 +0300

    Added lazyload package

--- frontend/src/components/custom/index.js
+++ frontend/src/components/custom/index.js
@@ -1,7 +1,9 @@
 import VueClickAway from "vue3-click-away";
 import Popper from "vue3-popper";
+import VueLazyLoad from 'vue3-lazyload';
 
 export default [
   Popper,
-  VueClickAway
+  VueClickAway,
+  VueLazyLoad
 ];
